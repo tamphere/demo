c19b94d372eb316e4ecdcc07dc22ddcf02032b38
thianhmaipham <thianhmaip@gmail.com>
2021-02-02 21:17:51 +0100

    Version 1.1

--- app.py
+++ app.py
@@ -1,63 +1,63 @@
-from flask import Flask, render_template, request  # never name your document flask because it will refer to the document instead of the library and the import fails
+from flask import Flask, render_template, redirect, url_for, request  # never name your document flask because it will refer to the document instead of the library and the import fails
 
 app = Flask(__name__)
 
-opt = []
-cri = []
-scr = []
+opt = ['Get a dog','Get a cat']
+cri = ['Time','Cost']
+f_score = {}
+scr = {}
 
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("index.html", cri=cri, opt=opt, scr=scr, f_score=f_score)
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/option", methods=['POST'])
 def option():
     if request.method == 'POST':
         new_option = request.form['option']
         opt.append(new_option)
-        return render_template("index.html", cri=cri, opt=opt)
-    else:
-        return render_template("test.html")
+        return redirect(url_for('home'))
 
-
-@app.route("/criteria", methods=['POST', 'GET'])
+@app.route("/criteria", methods=['POST'])
 def criteria():
     if request.method == 'POST':
         new_criteria = request.form['criteria']
         cri.append(new_criteria)
-        return render_template("index.html", cri=cri, opt=opt)
-    else:
-        return render_template("test.html")
+        return redirect(url_for('home'))
 
 
-@app.route("/score", methods=['POST', 'GET'])
+@app.route("/score", methods=['POST'])
 def score():
     if request.method == 'POST':
         new_score = request.form['score']
         cell_cri = request.form['cell_cri']
         cell_opt = request.form['cell_opt']
-        print(new_score)
-        print(cell_cri)
-        print(cell_opt)
-        scr.append([cell_cri, cell_opt, new_score])
+        if cell_opt not in scr:
+            scr[cell_opt] = {}
+        scr[cell_opt][cell_cri] = new_score
         print(scr)
-        return render_template("index.html", scr_element=new_score, cri=cri, opt=opt, scr=scr)
-    else:
-        return render_template("test.html")
+
+    for k, v in scr.items():
+        if k == cell_opt:
+            l = list(v.values())
+            s = sum([int(i) for i in l])
+            f_score[cell_opt] = s
+
+        return redirect(url_for('home'))
 
 
-@app.route("/final_score", methods=['POST', 'GET'])
+@app.route("/final_score", methods=['POST'])
 def final_score():
     if request.method == 'POST':
         cell_opt = request.form['cell_opt']
-        opt_scr_lst = []
-        for i, j in list(enumerate(scr)):
-            if cell_opt in j:
-                opt_scr = scr[i][2]
-                opt_scr_lst.append(opt_scr)
+        for k, v in scr.items():
+            if k == cell_opt:
+                l = list(v.values())
+                s = sum([int(i) for i in l])
+                f_score[cell_opt] = s
+        print(f_score)
 
-        return render_template('index.html', final_score=str(sum((int(i) for i in opt_scr_lst))), cri=cri, opt=opt,
-                               cell_opt=cell_opt)
-    else:
-        return render_template("test.html")
-
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)  # debug=True so that the page can be updated without having to refresh the server
